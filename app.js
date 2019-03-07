1d482962d2a19e77f25a818d00285c52b5e2fc5a
Jack Bird <jack.bird@durham.ac.uk>
2019-03-07 09:58:11 +0000

    Added the node-fetch dependency

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 
-app.get('/', function(req, resp){
-   resp.send('Hello world')
+app.use("/", express.static("client"));
+
+app.get("/api", function(req, resp){
+   resp.send("Hello world")
 });
 
 module.exports = app;
