6c185a2645b265da5d4127dd76cc707f94f58ef9
Jack Bird <jack.bird@durham.ac.uk>
2019-03-10 21:26:50 +0000

    Added the images to the query results

--- server/wikiAPI.js
+++ server/wikiAPI.js
@@ -1,9 +1,14 @@
 const fetch = require("node-fetch");
 
 const search = function(query, callback) {
-	let searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="
-	let pagesURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvsection=0&pageids="
-	searchURL += query
+	let searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";0
+	let pagesURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvsection=0&pageids=";
+	let imagesURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&piprop=name|thumbnail&pithumbsize=300&pageids=";
+	searchURL += query;
+
+	let pageids = [];
+
+	let results = {};
 
 	fetch(searchURL)
 	.then(response => response.text())
@@ -11,7 +16,7 @@ const search = function(query, callback) {
 		// Get all the search results as an array
 		let results = JSON.parse(body).query.search;
 
-		// Now search through the search results for people
+		// Build the query to get page information
 		for(let i = 0, l = results.length; i < l - 1; i += 1) {
 			pagesURL += results[i].pageid + "|"
 		}
@@ -23,17 +28,77 @@ const search = function(query, callback) {
 	.then(body => {
 		// Get the page infomation
 		let pages = JSON.parse(body).query.pages;
-		pages = Object.values(pages)
+		pages = Object.values(pages);
 
 		for(let i = 0, length = pages.length; i < length; i += 1) {
 			let page = pages[i];
 			// The wikitext
 			let content = page.revisions[0]["*"];
 
+			// Check if they are a person
 			if(content.indexOf("birth_date") != -1) {
-				console.log(page.title);
+				let result = {};
+
+				result.title = page.title;
+				result.id = page.pageid;
+
+				pageids.push(page.pageid)
+				
+				results[page.pageid] = result;
 			}
 		}
+
+		// Build query to get image information
+		for(let i = 0, l = pageids.length; i < l - 1; i += 1) {
+			imagesURL += pageids[i] + "|"
+		}
+		imagesURL += pageids[pageids.length - 1];
+
+		return fetch(imagesURL);
+	})
+	.then(response => response.text())
+	.then(body => {
+		let images = JSON.parse(body).query.pages;
+
+		let data = {
+			"status": 200,
+			"data": []
+		};
+
+		console.log(images)
+
+		for(let i = 0, l = pageids.length; i < l; i += 1) {
+			let result = {}
+
+			result.title = results[pageids[i]].title;
+			result.id = pageids[i];
+
+			let image
+			try {
+				image = images[pageids[i]].thumbnail.source
+			} catch(err) {
+				image = false
+			}
+
+			result.image = "";
+			if(image) {
+				result.image = image;
+			}
+
+			// console.log(result)
+
+			data.data.push(result);
+		}
+
+		callback(data)
+	})
+	.catch(err => {
+		let data = {
+			"status": 500,
+			"data": []
+		}
+
+		callback(data);
 	});
 }
 
