e956e6536d484752931f6d7c05b50f62dd2e5a79
Jack Bird <jack.bird@durham.ac.uk>
2019-03-25 12:19:49 +0000

    added a get recipe infomation function

--- server/recipes.js
+++ server/recipes.js
@@ -3,9 +3,7 @@ const recipes = require("./recipes.json");
 const people = require("./people.js");
 
 const search = function(query, callback) {
-	//one recipe per person
 	people.search(query, data => {
-		// console.log(data)
 		if(data.status != 200) {
 			callback({
 				status: data.status,
@@ -22,8 +20,8 @@ const search = function(query, callback) {
 
 		for(let i = 0, l = content.length; i < l; i += 1) {
 			let rng = seedrandom(content[i].id);
-
-			let randomIndex = Math.floor(rng()*recipesCount);
+			let random = rng()
+			let randomIndex = Math.floor(random*recipesCount);
 
 			let recipe = recipes.generator[randomIndex];
 
@@ -33,7 +31,28 @@ const search = function(query, callback) {
 			result.title = recipe.title.replace("(*)", content[i].title);
 			result.image = content[i].image != "" ? content[i].image : recipe.image;
 
-			result.description = content[i].description;
+
+			// Create the description
+			if(content[i].description == -1) {
+				let desc = "";
+
+				if(recipe.intro == "") {
+					desc = content[i].title + " in a delicous dish"
+				} else {
+					let intro = recipe.intro.replace('(*)', content[i].title);
+
+					let characterLimit = 150
+					if(intro.length > characterLimit) {
+						desc = intro.substring(0,characterLimit) + "...";
+					} else {
+						desc = intro
+					}
+				}
+
+				result.description = desc;
+			} else {
+				result.description = content[i].description
+			}
 
 			result.cooksIn = recipe.cooksIn;
 			result.serves = recipe.serves;
@@ -41,10 +60,73 @@ const search = function(query, callback) {
 			returnResults.push(result)
 		}
 
-		callback(returnResults)
+		callback({
+			status: 200,
+			data: returnResults
+		})
+	})
+}
+
+const getRecipe = function(id, callback) {
+	let recipesCount = recipes.generator.length;
+
+	let rng = seedrandom(parseInt(id));
+	let random = rng()
+	let randomIndex = Math.floor(random*recipesCount);
+
+	let recipe = recipes.generator[randomIndex];
+
+	let result = {};
+
+	people.getPageInfo(id, (data) => {
+		if(data.status != 200) {
+			callback({
+				status: data.status,
+				data: []
+			})
+			return
+		}
+
+		let content = data.data;
+
+		result.title = recipe.title.replace("(*)", content.title);
+		result.intro = recipe.intro;
+		result.foodImage = recipe.image;
+		result.personImage = content.image;
+		result.serves = recipe.serves;
+		result.cooksIn = recipe.cooksIn;
+
+		let ingredients = [];
+
+		for(let i = 0, ingred = recipe.ingredients, length = ingred.length; i < length; i += 1) {
+			let name = ingred[i].name.replace("(*)", content.title)
+			
+			ingredients.push({
+				name: name,
+				quantity: ingred[i].quantity
+			})
+		}
+
+		result.ingredients = ingredients;
+
+		let method = [];
+
+		for(let i = 0, rmethod = recipe.method, length = rmethod.length; i < length; i += 1) {
+			method.push(
+				rmethod[i].replace("(*)", content.title)
+			);
+		}
+
+		result.method = method;
+
+		callback({
+			status: 200,
+			data: result
+		})
 	})
 }
 
 module.exports = {
-	search: search
+	search: search,
+	getRecipe: getRecipe
 }
