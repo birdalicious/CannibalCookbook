6e92cc459d3db54c8a8860903946b3f7e15f2794
Jack Bird <jack.bird@durham.ac.uk>
2019-04-07 19:11:52 +0100

    Revert "conflicts"
    This reverts commit fb72f710ad934ab1c61bd1f52b0bf05668af992f.

---
+++ server/wikiFetch.js
@@ -0,0 +1,42 @@
+const fetch = require("node-fetch");
+
+function convertQueryArrayToString(queries) {
+	let string = ""
+	let length = queries.length
+	for(let i = 0; i < length - 1; i += 1) {
+		string += queries[i] + "|"
+	}
+	string += queries[length - 1];
+
+	return string
+}
+
+function searchFetch(query) {
+	let searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
+	return fetch(searchURL + query);
+}
+
+function pagesByIdFetch(queries) {
+	let pagesByIdURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvsection=0&pageids=";
+	let stringQueries = convertQueryArrayToString(queries);
+	return fetch(pagesByIdURL + stringQueries);
+}
+
+function pagesByTitleFetch(stringQueries) {
+	let pagesByTitleURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvsection=0&rvprop=content&titles=";
+	return fetch(pagesByTitleURL + stringQueries);
+}
+
+function imagesByIdFetch(queries) {
+	let imagesURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&piprop=name|thumbnail&pithumbsize=300&pageids=";
+	let stringQueries = convertQueryArrayToString(queries);
+	return fetch(imagesURL + stringQueries);
+}
+
+
+module.exports = {
+	searchFetch: searchFetch,
+	pagesByIdFetch: pagesByIdFetch,
+	pagesByTitleFetch: pagesByTitleFetch,
+	imagesByIdFetch: imagesByIdFetch
+}
